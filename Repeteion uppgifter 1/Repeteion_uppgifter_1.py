68a79ef8c2e8eef34ddb09f2b1d83ba2f8a3963b
Admin <Admin@TEB123008>
2022-11-11 09:13:27 +0100

    Add project files.

---
+++ Repeteion uppgifter 1/Repeteion_uppgifter_1.py
@@ -0,0 +1,10 @@
+print(4.2/(7.3+0.26),"\n")
+
+print(2**6,"\n")
+
+print(200**0.5,"\n")
+
+minuter=1100
+resten=minuter%60
+heltal=minuter/60
+print("Det gar", int(heltal),"timmar och", resten,"minuter pa", minuter,"minuter.\n")
